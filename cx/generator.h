6b4b738faf512db666a75dbe59bae77a0b3c9551
iulian <iulian.rusu999@gmail.com>
2021-07-03 21:13:44 +0300

    Make iterator inner class of generator

--- cx/generator.h
+++ cx/generator.h
@@ -2,7 +2,6 @@
 #define CX_GENERATOR_H
 
 #include "regex_result.h"
-#include "iterator.h"
 
 namespace cx
 {
@@ -14,19 +13,71 @@ namespace cx
     template<producer Prod>
     struct generator : Prod
     {
+        using value_type = std::invoke_result_t<Prod>;
+        
         template<typename Func>
-        constexpr explicit generator(Func &&func) noexcept(std::is_nothrow_move_constructible_v<Prod>)
+        constexpr explicit generator(Func &&func)
+        noexcept(std::is_nothrow_move_constructible_v<Prod>)
                 : Prod(std::forward<Func>(func))
         {}
+
+        /**
+         * Lazy iterator over the generated results.
+         * A result that is explicitly convertible to the boolean false
+         * signals iteration ending.
+         */
+        class iterator
+        {
+            generator<Prod> &gen;
+            value_type current_result;
+            bool active;
+        public:
+            template<typename Res>
+            constexpr explicit iterator(generator<Prod> &g, Res &&res, bool a)
+            noexcept(std::is_nothrow_move_constructible_v<Res>)
+                    : gen(g), current_result(std::forward<Res>(res)), active(a)
+            {}
+
+            constexpr explicit operator bool() const noexcept
+            {
+                return active;
+            }
+
+            value_type &operator*() noexcept
+            {
+                return current_result;
+            }
+
+            iterator &operator++() noexcept
+            {
+                current_result = std::move(gen());
+                active = static_cast<bool>(current_result);
+                return *this;
+            }
+
+            template<typename Iter>
+            bool operator==(Iter const &rhs) const noexcept
+            {
+                return static_cast<bool>(*this) == static_cast<bool>(rhs);
+            }
+
+            template<typename Iter>
+            bool operator!=(Iter const &rhs) const noexcept
+            {
+                return static_cast<bool>(*this) != static_cast<bool>(rhs);
+            }
+        };
+
+        struct end_iterator : std::bool_constant<false> {};
 
         auto begin() noexcept
         {
             auto initial_result = (*this)();
             auto initial_state = static_cast<bool>(initial_result);
-            return lazy_iterator{*this, std::move(initial_result), initial_state};
+            return iterator{*this, std::move(initial_result), initial_state};
         }
 
-        auto end() noexcept
+        auto end() const noexcept
         {
             return end_iterator{};
         }
